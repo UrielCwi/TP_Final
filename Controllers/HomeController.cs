c79c6b3b8408309135d10dc69851f6fbb395a458
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2023-10-30 10:10:42 -0300

    1.0.1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using TP_FINAL_TOPODIARIO.Models;
 namespace Topodiario.Controllers;
 
 public class HomeController : Controller
@@ -7,5 +7,16 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+    public IActionResult Login(string Nombre, string Contraseña)
+    {
+        ViewBag.Usuario = BD.LoginUsuario(Nombre, Contraseña);
+        return View("Calendario");
+    }
+    public IActionResult Registro(Usuarios Usuario)
+    {
+        BD.RegistrarUsuario(Usuario);
+        ViewBag.Usuario = BD.LoginUsuario(Usuario.Nombre, Usuario.Contraseña);
+        return View("Calendario");
     }
 }
