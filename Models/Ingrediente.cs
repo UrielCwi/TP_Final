c2d9859fc08d0fc77f69c61a4749471b4f613a05
eitan <mail@mail.com>
2024-09-16 12:14:56 -0300

    limita los caracteres y no acepta negativos

--- Models/Ingrediente.cs
+++ Models/Ingrediente.cs
@@ -2,7 +2,7 @@ public class Ingrediente
 {
     public int id { get; set; }
     public string descripcion { get; set; }
-    public string cantidad { get; set; }
+    public double cantidad { get; set; }
     public double valorUnidad { get; set; }
     public bool activo { get; set; }
     public int idUnidad { get; set; }
